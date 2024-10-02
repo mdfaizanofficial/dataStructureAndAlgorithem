e95fe34b709a3ea014d046fd2656240fb356f7c3
Faizan Furqan Ahmad <alamfaizan173@gmail.com>
2024-10-02 10:27:11 +0530

    Question 18

---
+++ Patterns/Question18.java
@@ -0,0 +1,18 @@
+public class Question18 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        char ch = 'E';
+        for(int i = 0; i < n; i++){
+            char temp = ch;
+            for(int j = 0; j <= i; j++){
+                System.out.print(temp);
+                temp++;
+            }
+
+            ch--;
+            System.out.println();
+
+        }
+    }
+}
